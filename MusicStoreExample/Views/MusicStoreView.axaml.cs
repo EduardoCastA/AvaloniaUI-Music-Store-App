4c2835acfffb39f946ddac326d4e7f46114ea687
EduardoCastA <a311946@alumnos.uaslp.mx>
2024-10-15 08:10:34 -0600

    Agregar contenido al dialog

---
+++ MusicStoreExample/Views/MusicStoreView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MusicStoreExample.Views
+{
+    public partial class MusicStoreView : UserControl
+    {
+        public MusicStoreView()
+        {
+            InitializeComponent();
+        }
+    }
+}
